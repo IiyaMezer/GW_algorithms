36ac4741b42ce48583515e47a7ecfe2ba856ee93
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-02-26 18:22:18 +0700

    Дз готово, но правильно ли?

---
+++ Lesson3/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Lesson3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> testList = new List<int>();
+
+            int num = 12;
+
+            Random random = new Random();
+
+            for (int i = 0; i < num; i++)
+            {
+                testList.Add(i);
+            }
+
+            ListPrint(testList);
+            ListReverse(testList);
+            ListPrint(testList);
+
+        }
+
+        private static void ListPrint(List<int> list)
+        {
+            foreach (var num in list)
+            {
+                Console.Write($"{num}"+" ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void ListReverse(List<int> list)
+        {
+            for (int i = 0; i < list.Count/2; i++)
+            {
+                (list[i], list[list.Count - i - 1]) = (list[list.Count - i - 1], list[i]);
+            }
+        }
+    }
+}
