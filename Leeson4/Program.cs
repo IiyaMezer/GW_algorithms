b89166cff908a5e51854dc1ef876e024d0c8950a
Elijah_M <102888192+IiyaMezer@users.noreply.github.com>
2023-03-11 15:44:34 +0700

    Испытания

--- Leeson4/Program.cs
+++ Leeson4/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var tree = new RBTree();
+            string input;
+            while (true)
+            {
+                Console.Write("Введите значение узла дерева: ");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out int result))
+                {
+                    tree.add(result);
+                    Console.WriteLine("done");
+                }
+                else
+                {
+                    if (input.Equals("exit"))
+                    {
+                        return;
+                    }
+                }
+            }
         }
     }
 }
